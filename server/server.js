9db10417bc9201660d0b0d0c08a7ff1739f21001
William Castillo <willcastpz@gmail.com>
2022-08-07 15:38:54 -0400

    added images endpoints to server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
 const PORT = 8000;
+// importing our handlers
+const { getImages, getImage } = require("./handlers/images/imagesHandlers");
 
-app.get("/", (req, res) => {
-  res.status(200).json({ status: 200, message: "its alive!!" });
-});
+// Calling express and adding .use config
+express()
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
+  .use(morgan("tiny"))
+  .use(express.static("public"))
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
 
-app.listen(PORT, () => {
-  console.log(`Server app listening on port ${PORT}`);
-});
+  // These are the images endpoints
+  .get("/images", getImages)
+  .get("/images/:imageId", getImage)
+
+  .listen(PORT, () => {
+    console.log(`Server app listening on port ${PORT}`);
+  });
