3c1cfa799bf2dcb6806e71baea4a484cd833ccf7
William Castillo <willcastpz@gmail.com>
2022-08-08 16:51:27 -0400

    added the get image endpoint

--- server/handlers/images/imagesHandlers.js
+++ server/handlers/images/imagesHandlers.js
@@ -6,6 +6,18 @@ const getImages = async (req, res) => {
   res.status(200).json({ status: 200, message: "success", data: images });
 };
 
-const getImage = async (req, res) => {};
+const getImage = async (req, res) => {
+  const { imageId } = req.params;
+  const _id = imageId;
+  const foundImage = await db.collection("images").findOne({ _id });
+
+  foundImage
+    ? res
+        .status(200)
+        .json({ status: 200, message: "success", data: foundImage })
+    : res
+        .status(404)
+        .json({ status: 404, message: `Image with id ${imageId} not found` });
+};
 
 module.exports = { getImages, getImage };
