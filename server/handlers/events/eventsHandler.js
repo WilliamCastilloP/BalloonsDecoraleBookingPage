df9c3b30de8c6a2ab983c2dc954b917346c0a857
William Castillo <willcastpz@gmail.com>
2022-08-09 19:27:07 -0400

    created new endpoints for events

--- server/handlers/events/eventsHandler.js
+++ server/handlers/events/eventsHandler.js
@@ -10,7 +10,7 @@ const addEvent = async (req, res) => {
     ? res
         .status(200)
         .json({ status: 200, message: "success", newEventObj, added })
-    : res.status(400).json({ status: 400, message: "error" });
+    : res.status(400).json({ status: 400, message: "error", data: req.body });
 };
 
 const getEvents = async (req, res) => {
@@ -19,4 +19,44 @@ const getEvents = async (req, res) => {
   res.status(200).json({ status: 200, message: "success", foundEvents });
 };
 
-module.exports = { addEvent, getEvents };
+const getEvent = async (req, res) => {
+  const _id = req.params.eventId;
+
+  const found = await db.collection("events").findOne({ _id });
+
+  res.status(200).json({ status: 200, message: "success", found });
+};
+
+const updateEvent = async (req, res) => {
+  const {
+    _id,
+    firstName,
+    lastName,
+    postalCode,
+    date,
+    theme,
+    colors,
+    description,
+  } = req.body;
+
+  const query = { _id };
+  const newValues = {
+    $set: { firstName, lastName, postalCode, date, theme, colors, description },
+  };
+  const updated = await db.collection("events").updateOne(query, newValues);
+  updated.acknowledged
+    ? res.status(200).json({ status: 200, message: "success", updated })
+    : res.status(400).json({ status: 400, message: "error", data: req.body });
+};
+
+const deleteEvent = async (req, res) => {
+  const { _id } = req.body;
+
+  const deleted = await db.collection("events").deleteOne({ _id });
+
+  deleted.acknowledged
+    ? res.status(200).json({ status: 200, message: "success", deleted, _id })
+    : res.status(400).json({ status: 400, message: "error", _id });
+};
+
+module.exports = { addEvent, getEvents, updateEvent, getEvent, deleteEvent };
