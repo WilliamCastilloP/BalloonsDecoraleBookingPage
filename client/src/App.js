d206a3173c683c7d8588b201a5da8a1e4c8cf5c7
William Castillo <willcastpz@gmail.com>
2022-08-16 14:58:33 -0400

    created an auth folder and files to manage auth

--- client/src/App.js
+++ client/src/App.js
@@ -8,49 +8,68 @@ import UpdatePage from "./pages/UpdatePage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import EventsPage from "./pages/EventsPage";
+import ConfirmationPage from "./pages/ConfirmationPage";
+import UserBookedDecoration from "./pages/UserBookedDecoration";
 import GlobalStyles from "./components/GlobalStyles";
 import styled from "styled-components";
 import "../src/calendar.css";
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
+import Auth0ContextProvider from "./context/AuthenticationContext";
+import ProtectedRoute from "./auth/protected-route";
 
 function App(props) {
   return (
     <BrowserRouter>
-      <GlobalStyles />
-      <Navbar />
-      <Wrapper>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route exact path="/gallery">
-            <GalleryPage />
-          </Route>
-          <Route exact path="/gallery/:imageId">
-            Image details
-          </Route>
-          <Route exact path="/booking/:imageId">
-            <BookingPage />
-          </Route>
-          <Route exact path="/signin">
-            <SignInPage />
-          </Route>
-          <Route exact path="/confirmation">
-            confirmation
-          </Route>
-          <Route exact path="/events">
-            <EventsPage />
-          </Route>
-          <Route exact path="/events/:eventId">
-            <UpdatePage />
-          </Route>
-        </Switch>
-      </Wrapper>
-      <Footer />
+      <Auth0ProviderWithHistory>
+        <Auth0ContextProvider>
+          <GlobalStyles />
+          <Navbar />
+          <Wrapper>
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route exact path="/gallery">
+                <GalleryPage />
+              </Route>
+              <Route exact path="/about">
+                {/* TODO AboutPage */}
+              </Route>
+              <ProtectedRoute
+                exact
+                path="/booking/:imageId"
+                component={BookingPage}
+              />
+              <Route exact path="/signin">
+                <SignInPage />
+              </Route>
+              <ProtectedRoute
+                exact
+                path="/confirmation"
+                component={ConfirmationPage}
+              />
+              <ProtectedRoute exact path="/events" component={EventsPage} />
+              <ProtectedRoute
+                exact
+                path="/events/:eventId"
+                component={UpdatePage}
+              />
+              <ProtectedRoute
+                exact
+                path="/booked-decoration"
+                component={UserBookedDecoration}
+              />
+            </Switch>
+          </Wrapper>
+          <Footer />
+        </Auth0ContextProvider>
+      </Auth0ProviderWithHistory>
     </BrowserRouter>
   );
 }
 
 const Wrapper = styled.div`
+  /* width: 100%; */
   display: flex;
   flex-direction: column;
   align-content: space-between;
--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Auth0Provider } from "@auth0/auth0-react";
-import Auth0ContextProvider from "./context/AuthenticationContext";
-import BookingProvider from "./context/BookingContext";
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+import BookingProvider from "./context/BookingContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    redirectUri={window.location.origin}
-  >
-    <Auth0ContextProvider>
-      <BookingProvider>
-        <App />
-      </BookingProvider>
-    </Auth0ContextProvider>
-  </Auth0Provider>
+  <BookingProvider>
+    <App />
+  </BookingProvider>
 );
