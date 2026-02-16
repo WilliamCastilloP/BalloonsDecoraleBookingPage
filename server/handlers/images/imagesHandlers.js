c78c6f51cf31301964bf6a895e192eb991733a8d
William Castillo <Willcastpz@gmail.com>
2026-02-16 17:07:15 -0500

    Refactor image handlers to use connectDB

--- server/handlers/images/imagesHandlers.js
+++ server/handlers/images/imagesHandlers.js
@@ -1,26 +1,49 @@
-const { db, client } = require("../../db/client");
+const { connectDB } = require("../../db/client");
+const { ObjectId } = require("mongodb");
 
-// retrieves all images in the db
+// GET ALL IMAGES
 const getImages = async (req, res) => {
-  await client.connect();
-  const images = await db.collection("images").find().toArray();
+  try {
+    const db = await connectDB();
+    const images = await db.collection("images").find().toArray();
 
-  res.status(200).json({ status: 200, message: "success", data: images });
+    res.status(200).json({
+      status: 200,
+      message: "success",
+      data: images,
+    });
+  } catch (err) {
+    console.error("getImages error:", err);
+    res.status(500).json({ status: 500, error: "Server error" });
+  }
 };
 
-// retrieves a single image by image ID
+// GET IMAGE
 const getImage = async (req, res) => {
-  const { imageId } = req.params;
-  const _id = imageId;
-  const foundImage = await db.collection("images").findOne({ _id });
+  try {
+    const db = await connectDB();
+    const { imageId } = req.params;
 
-  foundImage
-    ? res
-        .status(200)
-        .json({ status: 200, message: "success", data: foundImage })
-    : res
-        .status(404)
-        .json({ status: 404, message: `Image with id ${imageId} not found` });
+    const foundImage = await db
+      .collection("images")
+      .findOne({ _id: new ObjectId(imageId) });
+
+    if (!foundImage) {
+      return res.status(404).json({
+        status: 404,
+        message: `Image with id ${imageId} not found`,
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: "success",
+      data: foundImage,
+    });
+  } catch (err) {
+    console.error("getImage error:", err);
+    res.status(500).json({ status: 500, error: "Server error" });
+  }
 };
 
 module.exports = { getImages, getImage };
